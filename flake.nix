ae5e6d1bdc2c24e1a4ab8c5e5a4b2408466cab35
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2024-07-23 13:56:31 +0200

    initial import

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Go example flake for Zero to Nix";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2305.491812.tar.gz";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.buildGoModule {
+          name = "zero-to-nix-go";
+          src = ./.;
+          vendorSha256 = "sha256-Cy1/QqbO2MyYgqJZKxrt1FZzLSgXbhSK3ceFPUlFujw=";
+        };
+      });
+    };
+}
